84b9546c2e01e5d6997169d9b7ce2d2aa89557fe
Usman Irale <superirale@gmail.com>
2016-12-25 22:30:59 +0100

    bootstrap-sass added

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
+var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
 
 
